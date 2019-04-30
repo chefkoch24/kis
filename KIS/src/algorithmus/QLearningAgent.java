4a10530b09192863b167c76bff83abe6fec6f08f
chefkoch24 <kuennecke-felix@web.de>
2019-04-30 18:57:52 +0200

    Q Learning Agent

--- KIS/src/algorithmus/QLearningAgent.java
+++ KIS/src/algorithmus/QLearningAgent.java
@@ -11,23 +11,24 @@ public class QLearningAgent {
 	private static final int POSSIBLE_ACTIONS = 8;
 	boolean trained = false; 
 	/*
-	 * Mögliche Aktionen STOP = 0, DRIVE_FORWARD = 1, DRIVE_LEFT = 2,
+	 * possible actions: STOP = 0, DRIVE_FORWARD = 1, DRIVE_LEFT = 2,
 	 * DRIVE_RIGHT = 3, DRIVE_BACKWARD = 4, LOOK_FORWARD = 5, int LOOK_LEFT = 6,
 	 * LOOK_RIGHT = 7;
 	 */
 
-	// Abhängig von Abastand links, rechts, vorne, geschwindigkeit,
-	public QLearningAgent(int barrier, float speed) {
-		int s;
-		q = new double[s][POSSIBLE_ACTIONS];
+	// AbhÃ¤ngig von Abstand barriere, ort der barriere, geschwindigkeit,
+	// 3 mÃ¶gliche Orte der Barriere -> vorne = 0, links = 1, rechts = 2
+	public QLearningAgent(int barrierMax, int placeOfBarrier) {
+		int s = barrierMax + (placeOfBarrier * barrierMax);
+		this.q = new double[s][POSSIBLE_ACTIONS];
 
 	}
 
 	/**
-	 * Lernt durch die übergebenen Zustände, ob die Aktion erfolgreich war und
+	 * Lernt durch die Ã¼bergebenen ZustÃ¤nde, ob die Aktion erfolgreich war und
 	 * speichert die Werte in das q-array.
 	 * @param s: Aktueller Zustand
-	 * @param s_next: Nächster Zustand
+	 * @param s_next: NÃ¤chster Zustand
 	 * @param a: Aktion
 	 * @param r: Belohnung
 	 */
@@ -38,9 +39,9 @@ public class QLearningAgent {
 	}
 
 	/**
-	 * Wählt die Aktion mit der besten Rate aus
+	 * WÃ¤hlt die Aktion mit der besten Rate aus
 	 * @param s: Zustand s
-	 * @return: Gibt die Aktion als int zurück.
+	 * @return: Gibt die Aktion als int zurÃ¼ck.
 	 */
 	public int actionWithBestRating(int s) {
 		double max = 0;
@@ -55,19 +56,23 @@ public class QLearningAgent {
 	}	
 
 	/**
-	 * Wählt eine zufällige Aktion anhand des Zustands aus
+	 * WÃ¤hlt eine zufÃ¤llige Aktion anhand des Zustands aus
 	 * @param s: Zustand
-	 * @return: Gibt die Aktion als int zurück.
+	 * @return: Gibt die Aktion als int zurÃ¼ck.
 	 */
 	public int chooseAction(int s) {
 		int a = 0;
-		if (Math.random() < this.epsilon && trained == false) {
-			// Bound = 2, because we want 1 to be inclusive
-			a = ThreadLocalRandom.current().nextInt(-1, 2);
+		if (Math.random() < this.epsilon && this.trained == false) {
+			// Bound = 8, because we want 7 to be inclusive
+			a = ThreadLocalRandom.current().nextInt(0, 8);
 		} else {
-			a = actionWithBestRating(s) - 1;
+			a = actionWithBestRating(s);
 		}
 		return a;
 	}
+	
+	public int calculateState(int barrier, int barrierMax, int placeOfBarrier) {
+		return barrier + (placeOfBarrier * barrierMax);
+}
 
 }
