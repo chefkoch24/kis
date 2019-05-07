612a9c998a2f883c32a0212b61f9c5148f6196d8
student <student@192.168.101.20>
2019-05-07 12:21:37 +0200

    bump sensos woking

--- KIS/src/roboter/Start.java
+++ KIS/src/roboter/Start.java
@@ -4,7 +4,11 @@ public class Start {
 
 	public static void main(String[] args) {
 		LegoRoboter robot = new LegoRoboter();
-		robot.isBumped();
+		while(!robot.isBumped()){
+			System.out.println("not bumped");
+		}
+		System.out.println("bumped");
+		
 	}
 
 }
