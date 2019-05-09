b2cdb5943d44bbe078653c2eadc3ea3397cf863c
student <student@192.168.101.20>
2019-05-09 12:55:28 +0200

    Kommentar zur Wissenschaftlichen Ausarbeitung

--- KIS/src/roboter/Start.java
+++ KIS/src/roboter/Start.java
@@ -1,7 +1,16 @@
 package roboter;
 
+import lejos.hardware.Button;
+
 public class Start {
 
+	/**
+	 * 4 - 8 Seiten Wissenschaftliche Ausarbeitung
+	 * Mit Abstract, Einleitung, Auswertung der Versuche, Diskussion, Ausblick
+	 * Mögliche ähnliche wissenschaftliche Arbeiten, die sich mit diesem Thema 
+	 * beschäftigt hat.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		LegoRoboter robot = new LegoRoboter();
 //		TODO: forward = backward
@@ -10,6 +19,12 @@ public class Start {
 		robot.lookRight();
 		robot.lookRight();
 		robot.lookLeft();
+		robot.lookLeft();
+		robot.lookRight();
+		robot.lookRight();
+		robot.lookLeft();
+		System.out.println("Finished");
+		Button.waitForAnyPress();
 		/*while(!robot.isBumped()){
 			robot.forward();
 			System.out.println("not bumped");
